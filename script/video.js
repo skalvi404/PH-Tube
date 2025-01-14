7acd29e4f1ca95201630d58c40364e0cffc4b22b
sheikh-abdullah-al-alvi <sheikhabdullahalalvi221@gmail.com>
2025-01-14 18:50:07 +0600

    design and api intrigate for video section

--- script/video.js
+++ script/video.js
@@ -1,4 +1,4 @@
-// API FEATCH PROCESS
+// API FEATCH PROCESS FOR VIDEO BUTTON
 const loadCategories=()=>{
     fetch('https://openapi.programming-hero.com/api/phero-tube/categories')
     .then(res=>res.json())
@@ -8,6 +8,7 @@ const loadCategories=()=>{
     
     
 }
+
 // DISPLAY CATEGOREIS WITH LOOP
 
 function displayCategories(categories){
@@ -30,4 +31,68 @@ const showCategories=document.getElementById('categories')
 }
 
 
+// API FEATCH PROCESS FOR VIDEO 
+const loadVideos=()=>{
+    fetch('https://openapi.programming-hero.com/api/phero-tube/videos')
+    .then(res=>res.json())
+    .then(data=>displayVideos(data.videos))
+    .catch(error=>console.log(error))  
+    
+}
+
+// DISPLAY VIDEO WITH LOOP
+const displayVideos=(videos)=> {
+const videoContainer=document.getElementById('videos')
+videos.forEach(video=>{
+   
+    console.log(video);
+
+
+    const card=document.createElement("div");
+card.classList="card card-compact bg-base-100 shadow-xl"
+card.innerHTML=`
+  <figure>
+    <img
+      src=${video.thumbnail}
+      alt="Shoes" />
+  </figure>
+  <div class="card-body">
+    <h2 class="card-title">Shoes!</h2>
+    <p>If a dog chews shoes whose shoes does he choose?</p>
+    <div class="card-actions justify-end">
+      <button class="btn btn-primary">Buy Now</button>
+    </div>
+  </div>
+
+`
+videoContainer.append(card)
+    
+})
+
+
+
+
+ }
+
 loadCategories();
+loadVideos();
+
+const carddemo=
+{
+    "category_id": "1001",
+    "video_id": "aaal",
+    "thumbnail": "https://i.ibb.co/hdtZYbB/enchnting.jpg",
+    "title": "Enchanted Harmonies",
+    "authors": [
+        {
+            "profile_picture": "https://i.ibb.co/jh1q2F3/shopia.jpg",
+            "profile_name": "Sophia Williams",
+            "verified": false
+        }
+    ],
+    "others": {
+        "views": "7.6K",
+        "posted_date": "16450"
+    },
+    "description": "'Enchanted Harmonies' by Sophia Williams enchants listeners with its delicate, soothing sounds and melodic complexity. Garnering 7.6K views, this piece is perfect for those seeking an immersive musical experience that blends elegance with emotion, offering a unique soundscape that resonates deeply with its audience."
+}
